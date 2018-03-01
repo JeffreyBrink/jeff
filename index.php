86670b0c5d7e9f65d9d373d3442465244cea8c9c
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-03-01 16:54:59 -0700

    saving for end of day

--- index.php
+++ index.php
@@ -1,45 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="UTF-8"/>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+		<!-- Font Awesome -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="styles.css" type="text/css">
+
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
 		<title>Feeding Our Past</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.min.css'>
-		<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css'>
-		<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css'>
-		<link rel="stylesheet" href="styles.css">
+
 	</head>
 	<body>
+		<div class="container bg-secondary">
+			<div class="row">
+				testing testing
+			</div>
+		</div>
 
-		<div class="card-deck">
-			<div class="card">
-				<div class="card-body">
-					<div class="card card-inverse">
-						<img class="card-img" src="bg.png" alt="Card image">
-						<div class="card-img-overlay">
-					<h4 class="card-title">Card title</h4>
-					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+		<div class="container p-4 bg-info">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card mb-4">
+						<div class="card-body">
+							<h4 class="card-title">Card title</h4>
+							<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+								additional content. This content is a little bit longer.</p>
+						</div>
+					</div>
 				</div>
-			</div>
-			<div class="card">
-				<div class="card-body">
-					<div class="card card-inverse">
-						<img class="card-img" src="bg.png" alt="Card image">
-						<div class="card-img-overlay">
-					<h4 class="card-title">Card title</h4>
-					<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+					<div class="col-md-4">
+						<div class="card mb-4">
+							<div class="card-body">
+								<h4 class="card-title">Card title</h4>
+								<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+									additional content. This content is a little bit longer.</p>
+							</div>
+						</div>
+					</div>
+						<div class="col-md-4">
+							<div class="card mb-4">
+								<div class="card-body">
+									<h4 class="card-title">Card title</h4>
+									<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+										additional content. This content is a little bit longer.</p>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
-			</div>
-			<div class="card">
-				<div class="card-body">
-					<div class="card card-inverse">
-						<img class="card-img" src="http://selvaraj.me/codepen/images/hover-1.jpg" alt="Card image">
-						<div class="card-img-overlay">
-					<h4 class="card-title">Card title</h4>
-					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-				</div>
-			</div>
 
+		<div class="container mt-5">
+		<div class="row justify-content-center">
+			<div class="col-md-6">
+				<div class="card mb-4">
+					<div class="card-body">
+						<h4 class="card-title">Card title</h4>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+							additional content. This content is a little bit longer.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
 
 
 	</body>
