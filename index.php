a9fc12eba01807c3f9e36a96274ac49854e80238
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-28 21:49:52 -0700

    added images

--- index.php
+++ index.php
@@ -1,9 +1,103 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
-		<title>deployment practice</title>
+		<meta charset="UTF-8"/>
+		<title>Feeding Our Past</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.min.css'>
+		<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.4.0/animate.min.css'>
+		<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css'>
+		<link rel="stylesheet" href="styles.css">
 	</head>
 	<body>
-		<p>Practice practice practice</p>
+
+		<div class="container m-t-md">
+			<!-- First row -->
+			<div class="row">
+				<div class="col-xs-12 col-md-4">
+					<!-- Card -->
+					<article class="card animated fadeInLeft">
+						<img class="card-img-top img-responsive" src="org.png"/>
+						<div class="card-block">
+							<h4 class="card-title">Organization</h4>
+							<h6 class="text-muted">We could use some help</h6>
+							<a href="#" class="btn btn-primary">Read more</a>
+						</div>
+					</article><!-- .end Card -->
+				</div>
+				<div class="col-xs-12 col-md-4">
+					<!-- Card -->
+					<article class="card animated fadeInUp">
+
+						<img class="img-responsive" src="senior.png" alt="Leaf on the street" />
+						<h4 class="card-title">Senior</h4>
+						<div class="card-block">
+						</div>
+						<div class="card-block text-center">
+							<div class="btn-group hidden-sm-down hidden-md-down" role="group" aria-label="Card buttons">
+								<a href="#" class="btn btn-primary">I need assistance</a>
+								<a href="#" class="btn btn-primary">optional</a>
+							</div>
+							<div class="btn-group-vertical hidden-lg-up" role="group" aria-label="Card buttons">
+								<a href="#" class="btn btn-primary">I need assistance/a>
+									<a href="#" class="btn btn-primary">optional</a>
+							</div>
+						</div>
+					</article><!-- .end Card -->
+				</div>
+				<div class="col-xs-12 col-md-4">
+					<!-- Card -->
+					<article class="card animated fadeInRight">
+						<div class="card-block">
+							<h4 class="card-title">Volunteers</h4>
+							<h6 class="text-muted">I want to help</h6>
+							<div class="card-block text-center">
+								<div class="btn-group hidden-sm-down hidden-md-down" role="group" aria-label="Card buttons">
+									<a href="#" class="card-link">Find where to help</a>
+									<a href="#" class="card-link">One time donation</a>
+								</div>
+								<div class="btn-group-vertical hidden-lg-up" role="group" aria-label="Card buttons">
+									<a href="#" class="card-link">Find where to help</a><br />
+									<a href="#" class="card-link">One time donation</a>
+								</div>
+							</div>
+							<img class="card-img-bottom img-responsive" src="vol.png" alt="White sand" />
+					</article><!-- .end Card -->
+				</div>
+			</div><!-- .end First row -->
+			<!-- Second row -->
+			<div class="row m-t-md">
+				<div class="col-xs-12 col-md-4">
+					<!-- Card -->
+					<article class="card card-inverse animated fadeInLeft">
+						<div class="card-img-overlay">
+							<h4 class="card-title">Animal Farm</h4>
+							<h6 class="text-muted">George Orwell</h6>
+							<p class="card-text">Tired of their servitude to man, a group of farm animals revolt and establish their own society...</p>
+						</div>
+					</article><!-- .end Card -->
+				</div>
+				<div class="col-xs-12 col-md-4">
+					<!-- Card -->
+					<article class="card card-inverse animated fadeInDown">
+						<div class="card-img-overlay">
+							<h4 class="card-title">Life After Life</h4>
+							<h6 class="text-muted">Kate Atkinson</h6>
+							<p class="card-text">On a cold and snowy night, Ursula Todd is born, the 3rd child of a wealthy banker and his wife. She die...</p>
+						</div>
+					</article><!-- .end Card -->
+				</div>
+				<div class="col-xs-12 col-md-4">
+					<!-- Card -->
+					<article class="card card-inverse animated fadeInRight">
+						<div class="card-img-overlay">
+							<h4 class="card-title">Wind, Sand and Stars</h4>
+							<h6 class="text-muted">Antoine de Saint-Exupéry</h6>
+							<p class="card-text">Story that captures the grandeur, danger, and isolation of flight. Its exciting account of air adventure...</p>
+						</div>
+					</article><!-- .end Card -->
+				</div>
+			</div><!-- .end Second row -->
+
 	</body>
 </html>
