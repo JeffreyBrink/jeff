b23c28b1dadf4bfeed54b31c4ed3ecf61deaaaae
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-03-01 14:51:41 -0700

    resized bg img

--- index.php
+++ index.php
@@ -11,75 +11,36 @@
 	</head>
 	<body>
 
-		<div class="jumbotron jumbotron-fluid">
-			<div class="container m-t-md">
-				<!-- First row -->
-				<div class="row">
-					<div class="col-xs-12 col-md-4">
-						<!-- Card -->
-						<article class="card animated fadeInLeft">
+		<div class="card-deck">
+			<div class="card">
+				<div class="card-body">
+					<div class="card card-inverse">
+						<img class="card-img" src="bg.png" alt="Card image">
+						<div class="card-img-overlay">
+					<h4 class="card-title">Card title</h4>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-body">
+					<div class="card card-inverse">
+						<img class="card-img" src="bg.png" alt="Card image">
+						<div class="card-img-overlay">
+					<h4 class="card-title">Card title</h4>
+					<p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+				</div>
+			</div>
+			<div class="card">
+				<div class="card-body">
+					<div class="card card-inverse">
+						<img class="card-img" src="http://selvaraj.me/codepen/images/hover-1.jpg" alt="Card image">
+						<div class="card-img-overlay">
+					<h4 class="card-title">Card title</h4>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+				</div>
+			</div>
 
-							<img class="card-img-top img-responsive" src="senior.png" alt="Deer in nature" />
-							<div class="card-block">
-								<div class="card-image-overlay">
-								<h4 class="card-title">Oragnization</h4>
-								<h6 class="text-muted">We need help</h6>
-								<a href="#" class="btn btn-primary">Read more</a>
-							</div>
-							</div>
-						</article><!-- .end Card -->
-					</div>
-					<div class="col-xs-12 col-md-4">
-						<!-- Card -->
-						<article class="card animated fadeInUp">
-							<div class="card-block">
-								<h4 class="card-title">senior</h4>
-								<h6 class="text-muted">words</h6>
-							</div>
-							<img class="img-responsive" src="senior.png" alt="Leaf on the street" />
-							<div class="card-block">
-							</div>
-							<div class="card-block text-center">
-								<div class="btn-group hidden-sm-down hidden-md-down" role="group" aria-label="Card buttons">
-									<a href="#" class="btn btn-primary">Read more</a>
-									<a href="#" class="btn btn-primary">Save for later</a>
-								</div>
-								<div class="btn-group-vertical hidden-lg-up" role="group" aria-label="Card buttons">
-									<a href="#" class="btn btn-primary">Read more</a>
-									<a href="#" class="btn btn-primary">Save for later</a>
-								</div>
-							</div>
-						</article><!-- .end Card -->
-					</div>
-					<div class="col-xs-12 col-md-4">
-						<!-- Card -->
-						<article class="card animated fadeInRight">
-							<div class="card-block">
-								<h4 class="card-title">volunteer</h4>
-								<h6 class="text-muted">I want to help</h6>
-							</div>
-							<div class="card-block text-center">
-								<div class="btn-group hidden-sm-down hidden-md-down" role="group" aria-label="Card buttons">
-									<a href="#" class="card-link">Read more</a>
-									<a href="#" class="card-link">Save for later</a>
-								</div>
-								<div class="btn-group-vertical hidden-lg-up" role="group" aria-label="Card buttons">
-									<a href="#" class="card-link">Read more</a><br />
-									<a href="#" class="card-link">Save for later</a>
-								</div>
-							</div>
-							<img class="card-img-bottom img-responsive" src="vol.png" alt="spacesaver" />
-						</article><!-- .end Card -->
-					</div>
-				</div>
-				<!-- Card with Center Text Alignment -->
-				<div class="container m-t-md">
-				<div class="card" style="width:20rem;">
-					<div class="card-body text-center">
-						<h4 class="card-title">Card Title</h4>
-						<p class="card-text">This is a card with center text alignment.</p>
-						<a href="#" class="btn btn-primary">Button</a>
-					</div>
-				</div>
+
+
 	</body>
 </html>
